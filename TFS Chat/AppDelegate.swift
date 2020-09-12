bf9de67e34c18b7b2594014f3584f8c670dea3df
dmitry <akatiev.d@gmail.com>
2020-09-12 21:11:07 +0300

    add logging for app lifecycle

--- TFS Chat/AppDelegate.swift
+++ TFS Chat/AppDelegate.swift
@@ -15,8 +15,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        printStateInfo(from: .notRunning, to: .inactive, methodName: #function)
         return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        printStateInfo(from: .inactive, to: .active, methodName: #function)
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        printStateInfo(from: .active, to: .inactive, methodName: #function)
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        printStateInfo(from: .inactive, to: .background, methodName: #function)
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        printStateInfo(from: .background, to: .inactive, methodName: #function)
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        printStateInfo(from: .background, to: .notRunning, methodName: #function)
+    }
+    
+    private func printStateInfo(from: AppState, to: AppState, methodName: String) {
+        print("Method \(methodName) was called. Application moved from '\(from)' to '\(to)' state. \n")
     }
     
 }
 
+enum AppState {
+    case notRunning, inactive, active, background, suspended
+}
