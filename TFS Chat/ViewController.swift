6bf760b595d9cff9d7db7d36d3af8b62a04ec243
dmitry <akatiev.d@gmail.com>
2020-09-18 15:52:24 +0300

    prettify profile screen layout

--- TFS Chat/ViewController.swift
+++ TFS Chat/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
    
     @IBOutlet var profileLogoView: UIView!
     @IBOutlet var saveButton: UIButton!
