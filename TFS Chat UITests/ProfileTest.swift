3b0b74f70963079b96c09b47db93934a950133ef
dmitry <akatiev.d@gmail.com>
2020-12-01 01:34:15 +0300

    stabilize ProfileTest

--- TFS Chat UITests/ProfileTest.swift
+++ TFS Chat UITests/ProfileTest.swift
@@ -14,7 +14,7 @@ class ProfileTest: XCTestCase {
         continueAfterFailure = false
     }
     
-    func testTextFieldsCount() throws {
+    func testTextInputFieldsCount() throws {
         let expectedCount = 2
         let defaultTimeout = 3.0
         let app = XCUIApplication()
@@ -23,11 +23,24 @@ class ProfileTest: XCTestCase {
         let profileLogo = app.otherElements["profileLogo"]
         _ = profileLogo.waitForExistence(timeout: defaultTimeout)
         profileLogo.tap()
-        _ = app.navigationBars["Profile"].waitForExistence(timeout: defaultTimeout)
+        _ = app.navigationBars["My Profile"].waitForExistence(timeout: defaultTimeout)
         
-        let actualCount = app.textFields.count + app.textViews.count
+        let actualCount = calculateTextInputFieldsCount(for: app)
         XCTAssertTrue(actualCount == expectedCount,
                       "Expected \(expectedCount) text elements, but was \(actualCount)")
+    }
+    
+    private func calculateTextInputFieldsCount(for app: XCUIApplication) -> Int {
+        var textViewsCount = app.textViews.count
+        for i in 0..<app.textViews.count {
+            let element = app.textViews.element(boundBy: i)
+            if element.label != "" {
+                textViewsCount -= 1
+                //each line with line break counts as separate textView with label, so we need to ignore such textViews
+            }
+        }
+        
+        return textViewsCount + app.textFields.count
     }
     
 }
